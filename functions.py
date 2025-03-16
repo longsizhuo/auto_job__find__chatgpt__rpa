37434dd7ea530ba26a53941cf52fcf070ee66175
Loong Loong <longsizhuo@gmail.com>
2025-03-17 09:14:57 +1100

    Update the API

--- functions.py
+++ functions.py
@@ -1,8 +1,10 @@
 import json
 import os
+
 from openai import OpenAI
 from prompts import assistant_instructions
 from dotenv import load_dotenv
+
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
@@ -11,40 +13,52 @@ OPENAI_BASE_URL = os.getenv('OPENAI_BASE_URL')
 # Init OpenAI Client
 client = OpenAI(api_key=OPENAI_API_KEY, base_url=OPENAI_BASE_URL)
 
+
 # Create or load assistant
 def create_assistant(client):
-  assistant_file_path = 'assistant.json'
+    assistant_file_path = 'assistant.json'
 
-  # If there is an assistant.json file already, then load that assistant
-  if os.path.exists(assistant_file_path):
-    with open(assistant_file_path, 'r') as file:
-      assistant_data = json.load(file)
-      assistant_id = assistant_data['assistant_id']
-      print("Loaded existing assistant ID.")
-  else:
-    # If no assistant.json is present, create a new assistant using the below specifications
+    # If there is an assistant.json file already, then load that assistant
+    if os.path.exists(assistant_file_path):
+        with open(assistant_file_path, 'r') as file:
+            assistant_data = json.load(file)
+            assistant_id = assistant_data['assistant_id']
+            print("Loaded existing assistant ID.")
+    else:
+        # If no assistant.json is present, create a new assistant using the below specifications
 
-    # To change the knowledge document, modify the file name below to match your document
-    # If you want to add multiple files, paste this function into ChatGPT and ask for it to add support for multiple files
-    file = client.files.create(file=open("my_cover.pdf", "rb"),
-                               purpose='assistants')
+        # To change the knowledge document, modify the file name below to match your document If you want to add
+        # multiple files, paste this function into ChatGPT and ask for it to add support for multiple files
+        # file = client.files.create(file=open("resume/my_cover.pdf", "rb"),
+        #                            purpose='assistants')
+        vector_store = client.vector_stores.create(name="My Resume")
+        print(vector_store.id)
+        file_streams = [open("resume/my_cover.pdf", "rb")]
+        # Use the upload and poll SDK helper to upload the files, add them to the vector store,
+        # and poll the status of the file batch for completion.
+        file_batch = client.vector_stores.file_batches.upload_and_poll(
+            vector_store_id=vector_store.id, files=file_streams
+        )
 
-    assistant = client.beta.assistants.create(
-        # Getting assistant prompt from "prompts.py" file, edit on left panel if you want to change the prompt
-        instructions=assistant_instructions,
-        model="gpt-3.5-turbo-1106",
-        tools=[
-            {
-                "type": "retrieval"  # This adds the knowledge base as a tool
-            },
-        ],
-        file_ids=[file.id])
+        print(file_batch.status)
+        print(file_batch.file_counts)
+        # New version of the assistant
+        assistant = client.beta.assistants.create(
+            instructions=assistant_instructions,
+            model="gpt-4o",
+            tools=[{"type": "file_search"}],
+        )
 
-    # Create a new assistant.json file to load on future runs
-    with open(assistant_file_path, 'w') as file:
-      json.dump({'assistant_id': assistant.id}, file)
-      print("Created a new assistant and saved the ID.")
+        assistant = client.beta.assistants.update(
+            assistant_id=assistant.id,
+            tool_resources={"file_search": {"vector_store_ids": [vector_store.id]}},
+        )
 
-    assistant_id = assistant.id
+        # Create a new assistant.json file to load on future runs
+        with open(assistant_file_path, 'w') as file:
+            json.dump({'assistant_id': assistant.id}, file)
+            print("Created a new assistant and saved the ID.")
 
-  return assistant_id
+        assistant_id = assistant.id
+
+    return assistant_id
